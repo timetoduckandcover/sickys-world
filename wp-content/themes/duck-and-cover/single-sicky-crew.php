e0768f548a0e6a993f90814ab0ea8b08077fe328
Liam Tarpey <liam.m.tarpey@gmail.com>
2016-04-13 08:56:40 +0100

    build: sicky crew single

--- wp-content/themes/duck-and-cover/single-sicky-crew.php
+++ wp-content/themes/duck-and-cover/single-sicky-crew.php
@@ -1,28 +1,22 @@
-<?php
-get_header();
-?>
-
-<div class="container">
-  <div class="row">
-      <main id="main" class="site-main col-xs-12 <?php echo esc_attr( ( 'no-sidebar' != FaceWPC()->get( 'facewp_abbey_sidebar_position' ) && is_active_sidebar( FaceWPC()->get( 'facewp_abbey_sidebar' ) ) ) ? 'col-md-9' : '' ); ?>">
-
-          <?php while ( have_posts() ) : the_post(); ?>
-            <?php
-    					$image_id = get_field('image');
-    					$image_size = 'thumb-size';
-    					$image_array = wp_get_attachment_image_src($image_id, $image_size);
-    					$image_url = $image_array[0];
-  					?>
-            <img src="<?php echo $image_url; ?>" alt="<?php echo $image['alt']; ?>" class="img-full"/>
-            <h1><?php the_field('name');?></h1>
-            <h3><?php the_field('job_title');?></h3>
-            <p>
-              <?php the_content();?>
-            </p>
-
-          <?php endwhile; // End of the loop. ?>
-
-      </main><!-- #main -->
-    </div>
-</div>
+<?php get_header(); ?>
+<main id="main" class="site-main">
+  <div class="sicky-single">
+    <?php while ( have_posts() ) : the_post(); ?>
+      <?php
+				$image_id = get_field('image');
+				$image_size = 'single-size';
+				$image_array = wp_get_attachment_image_src($image_id, $image_size);
+				$image_url = $image_array[0];
+			?>
+      <div class="sicky-single_hero">
+        <img src="<?php echo $image_url; ?>" alt="<?php echo $image['alt']; ?>" class="img-full" />
+      </div>
+      <div class="container">
+        <h1><?php the_field('name');?></h1>
+        <h3><?php the_field('job_title');?></h3>
+        <p><?php the_content();?></p>
+      </div>
+    <?php endwhile; // End of the loop. ?>
+  </div>
+</main><!-- #main -->
 <?php get_footer(); ?>
