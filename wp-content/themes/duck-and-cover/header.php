bba386cff47fc8e3729d8088b63bda6f4b80c9b5
Liam Tarpey <liam.m.tarpey@gmail.com>
2016-04-28 08:56:23 +0100

    build

--- wp-content/themes/duck-and-cover/header.php
+++ wp-content/themes/duck-and-cover/header.php
@@ -114,29 +114,6 @@ $facewp_core->set( 'facewp_abbey_sidebar', facewp_abbey_get_sidebar() );
 
             <?php endif; ?>
         </div>
-        <div class="container breadcrumb-row">
-            <div class="row">
-                <div class="col-md-7 start-xs">
-                    <?php if ( Kirki::get_option( 'facewp', 'site_breadcrumb_enable' ) == 1 ) : ?>
-                        <?php woocommerce_breadcrumb(); ?>
-                    <?php endif; ?>
-                </div>
-
-                <?php if ( have_posts() ) : ?>
-                    <div class="col-md-5 end-xs">
-                        <?php
-                        /**
-                         * woocommerce_before_shop_loop hook
-                         *
-                         * @hooked woocommerce_result_count - 20
-                         * @hooked woocommerce_catalog_ordering - 30
-                         */
-                        do_action( 'woocommerce_before_shop_loop' );
-                        ?>
-                    </div>
-                <?php endif; ?>
-            </div>
-        </div>
     <?php elseif ( function_exists( 'is_product' ) && is_product() ) : ?>
         <div class="container wide navigation-shop">
             <div class="row row-xs-center">
--- wp-content/themes/duck-and-cover/woocommerce/archive-product.php
+++ wp-content/themes/duck-and-cover/woocommerce/archive-product.php
@@ -29,7 +29,31 @@ get_header( 'shop' ); ?>
 
 		<?php if ( have_posts() ) : ?>
 
-            <?php woocommerce_product_loop_start(); ?>
+			<div class="container breadcrumb-row">
+					<div class="row">
+							<div class="col-md-7 start-xs">
+									<?php if ( Kirki::get_option( 'facewp', 'site_breadcrumb_enable' ) == 1 ) : ?>
+											<?php woocommerce_breadcrumb(); ?>
+									<?php endif; ?>
+							</div>
+
+							<?php if ( have_posts() ) : ?>
+									<div class="col-md-5 end-xs">
+											<?php
+											/**
+											 * woocommerce_before_shop_loop hook
+											 *
+											 * @hooked woocommerce_result_count - 20
+											 * @hooked woocommerce_catalog_ordering - 30
+											 */
+											do_action( 'woocommerce_before_shop_loop' );
+											?>
+									</div>
+							<?php endif; ?>
+					</div>
+			</div>
+
+      <?php woocommerce_product_loop_start(); ?>
 
 				<?php while ( have_posts() ) : the_post(); ?>
 
